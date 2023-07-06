dd1eb9907185730f0fca3421792e0074c2384e92
Alexander <78783703+AlexanderDevelopment@users.noreply.github.com>
2023-07-06 18:16:37 +0600

    Update NullChecker.cs

--- Solutions/NullChecker/NullChecker.cs
+++ Solutions/NullChecker/NullChecker.cs
@@ -1,6 +1,53 @@
-public static class NullChecker
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace _src.Scripts.OpenUnitySolutions
+{
+	public static class NullChecker
 	{
-		public static bool CheckForNull<T>(params T[] classes)
+		public static bool CheckForNull<T>(T checkingClass)
+		{
+			if (checkingClass is not null)
+				return true;
+
+			UnityEngine.Debug.LogError($"{checkingClass} is null");
+
+			return false;
+		}
+
+
+		public static bool CheckForNull<T>(T checkingClass, GameObject notify)
+		{
+			if (checkingClass is not null)
+				return true;
+
+			UnityEngine.Debug.LogError($"{checkingClass} is null", notify);
+
+			return false;
+		}
+
+
+		public static bool CheckForNullAll<T>(GameObject notify, params T[] classes)
+		{
+			if (classes.Length <= 0)
+				return true;
+
+			foreach (var classExample in classes)
+			{
+				if (classExample is not null)
+					continue;
+
+				UnityEngine.Debug.LogError($"{classExample} is null", notify);
+
+				return false;
+			}
+
+			return true;
+		}
+
+
+		public static bool CheckForNullAll<T>(params T[] classes)
 		{
 			if (classes.Length <= 0)
 				return true;
@@ -11,8 +58,11 @@ public static class NullChecker
 					continue;
 
 				UnityEngine.Debug.LogError($"{classExample} is null");
+
 				return false;
 			}
+
 			return true;
 		}
 	}
+}
