075baeed24c773925ec6fa086447f9531998f887
Alexander <78783703+AlexanderDevelopment@users.noreply.github.com>
2023-07-21 22:04:31 +0600

    Update GenericPooler.cs

--- Solutions/GenericPooler.cs
+++ Solutions/GenericPooler.cs
@@ -1,6 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+
+
+namespace _src.Scripts.OpenUnitySolutions.GenericPooler
+{
 	public class Pooler<T> where T : Component
 	{
 		private int _poolSize;
@@ -18,10 +22,13 @@ using UnityEngine;
 
 		private void Initialisation()
 		{
+			GameObject newPooler = new GameObject($"{typeof(T)} pooler");
+
 			for (int i = 0; i < _poolSize; i++)
 			{
 				GameObject newObj = new GameObject(typeof(T).Name + "_" + (i + 1));
 				T component = newObj.AddComponent<T>();
+				newObj.gameObject.transform.SetParent(newPooler.transform);
 				_poolCache.Add(component);
 			}
 		}
@@ -32,18 +39,32 @@ using UnityEngine;
 			foreach (var component in _poolCache.Where(component => !component.gameObject.activeInHierarchy))
 			{
 				component.gameObject.SetActive(true);
+				component.gameObject.transform.position = Vector3.zero;
 				return component;
 			}
+
+			return null;
+		}
+		
+		public T GetFromPool(Vector3 position)
+		{
+			foreach (var component in _poolCache.Where(component => !component.gameObject.activeInHierarchy))
+			{
+				component.gameObject.SetActive(true);
+				component.gameObject.transform.position = position;
+				return component;
+			}
+
 			return null;
 		}
 
 
 		public void ReturnToPool(T component, int index)
 		{
-			if (index < 0 || index >= _poolSize)
-
-				UnityEngine.Debug.LogError("Index out of bounds");
-
-			component.gameObject.SetActive(false);
+			if (_poolCache.Contains(component))
+				component.gameObject.SetActive(false);
+			else
+				UnityEngine.Debug.LogError("This component not be initialized in Pooler", component.gameObject);
 		}
 	}
+}
