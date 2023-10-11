9803d3f73e4725ae4a9a5e0be46aeeac7d984b92
Alexander <78783703+AlexanderDevelopment@users.noreply.github.com>
2023-10-11 12:29:37 +0200

    Update AnimatorExtensionsMethods.cs

--- Solutions/ExtensionMethods/AnimatorExtensionsMethods.cs
+++ Solutions/ExtensionMethods/AnimatorExtensionsMethods.cs
@@ -1,1 +1,13 @@
-
+public static class AnimatorExtensionMethods
+	{
+		public static async UniTask AsyncCrossFadeGesture(this Animator animator, string animationName, float fadeInDuration)
+		{
+			animator.CrossFade(animationName, fadeInDuration);
+			while (animator.GetCurrentAnimatorStateInfo(0).IsName(animationName) && 
+			       animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+			{
+				await UniTask.Delay(1);
+			}
+			
+		}
+	}
