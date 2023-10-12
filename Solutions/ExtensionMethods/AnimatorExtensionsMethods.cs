37981489822e85be9f0e26a4b9051a3fa32021a7
Alexander <78783703+AlexanderDevelopment@users.noreply.github.com>
2023-10-12 16:10:53 +0200

    Update AnimatorExtensionsMethods.cs

--- Solutions/ExtensionMethods/AnimatorExtensionsMethods.cs
+++ Solutions/ExtensionMethods/AnimatorExtensionsMethods.cs
@@ -1,4 +1,5 @@
-public static class AnimatorExtensionMethods
+
+	public static class AnimatorExtensionMethods
 	{
 		public static async UniTask AsyncCrossFadeGesture(this Animator animator, string animationName, float fadeInDuration)
 		{
@@ -9,5 +10,23 @@ public static class AnimatorExtensionMethods
 				await UniTask.Delay(1);
 			}
 			
+		}
+
+
+		public static async UniTask PlayAnimationClipOnceWithNewController(this Animator animator, AnimationClip animation, float fadeInDuration)
+		{
+			var graph = PlayableGraph.Create();
+			var output = AnimationPlayableOutput.Create(graph, "Animation", animator);
+			var clipPlayable = AnimationClipPlayable.Create(graph, animation);
+			output.SetSourcePlayable(clipPlayable);
+
+			graph.Play();
+
+			await UniTask.WaitUntil(() => 
+				clipPlayable.GetTime() >= animation.length
+			);
+
+			graph.Stop();
+			graph.Destroy();
 		}
 	}
