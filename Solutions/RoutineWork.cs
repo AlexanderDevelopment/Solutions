1fecbbd75e9ed977f6b207f6fbdfd5706e12a9e1
Alexander <78783703+AlexanderDevelopment@users.noreply.github.com>
2024-11-26 16:51:12 +0100

    Update RoutineWork.cs

--- Solutions/RoutineWork.cs
+++ Solutions/RoutineWork.cs
@@ -4,17 +4,17 @@ namespace _src.Scripts.Utils
 {
     public static class RoutineWork
     {
-        public static void InitializeComponentFromGameObject<T>(GameObject parent, ref T component) where T : Component
+        public static void InitializeComponentFromGameObject<T>(GameObject gameObject, ref T component) where T : Component
         {
             if (component != null)
             {
                 Debug.LogWarning($"Component of type {typeof(T)} already received");
             }
 
-            component = parent.GetComponent<T>();
+            component = gameObject.GetComponent<T>();
             if (component == null)
             {
-                Debug.LogError($"Component of type {typeof(T)} not found in {parent.name}", parent);
+                Debug.LogError($"Component of type {typeof(T)} not found in {gameObject.name}", gameObject);
             }
         }
 
